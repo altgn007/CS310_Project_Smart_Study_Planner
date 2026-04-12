2690c6dafa039738098b6ec35618211e9c83f8dd
helincesxyz <helincesxyz>
2026-04-13 02:00:28 +0300

    change frame

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,32 @@ class SmartStudyPlannerApp extends StatelessWidget {
       routes: {
         SplashScreen.routeName: (context) => const SplashScreen(),
         OnboardingScreen.routeName: (context) => const OnboardingScreen(),
-        CreateAccountScreen.routeName: (context) =>
-            const CreateAccountScreen(),
+        CreateAccountScreen.routeName: (context) => const CreateAccountScreen(),
         LoginScreen.routeName: (context) => const LoginScreen(),
+      },
+      builder: (context, child) {
+        if (MediaQuery.of(context).size.width <= 500) return child!;
+        return Scaffold(
+          backgroundColor: const Color(0xFF1A1A1A),
+          body: Center(
+            child: Container(
+              width: 390,
+              height: 844,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 60,
+                    spreadRadius: 10,
+                  ),
+                ],
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: child,
+            ),
+          ),
+        );
       },
     );
   }
